31443ef30b9bd729335bb0c1f109a2f347803d6a
Andres <polachgg@gmail.com>
2017-06-28 15:59:33 -0300

    dsvsdvsd
    vsdvsdv

---
+++ envioxturnos.php
@@ -0,0 +1,27 @@
+<?php  
+if (!isset($_POST['enviar'])) {
+	echo "string";
+}
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$cobertura = $_POST['cobertura'];
+$numafiliado = $_POST['numafiliado'];
+$numdni = $_POST['numdni'];
+$telefono = $_POST['telefono'];
+$especialidad = $_POST['especialidad'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
